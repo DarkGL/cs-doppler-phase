dce4a5bc2998052cc80d203277c1c1c8daefef48
Rafał Więcek <gitzzz14@gmail.com>
2025-07-06 13:16:26 +0200

    Fix doppler phase mapping error

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,1 @@
-export declare function getDopplerPhase(iconUrl: string): "phase1" | "phase2" | "phase3" | "phase4" | "ruby" | "sapphire" | "blackpearl" | "emerald" | null;
+export declare function getDopplerPhase(iconUrl: string): "phase1" | "phase2" | "phase3" | "phase4" | "sapphire" | "ruby" | "blackpearl" | "emerald" | null;
